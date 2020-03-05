56ae094e3f3c7b5b34ab3be6007684f5ebd70767
Dong Hyuck Kim <44930378+HiddenEvent@users.noreply.github.com>
2020-03-05 16:57:45 +0900

    객체생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,11 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<bool> answer = [];
+
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +70,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctAnswer = quizBrain.questionBank[questionNumber]
+                    .questionAnswer;
               },
             ),
           ),
@@ -79,12 +89,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnswer = quizBrain.questionBank[questionNumber]
+                    .questionAnswer;
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+
+        ),
       ],
     );
   }
